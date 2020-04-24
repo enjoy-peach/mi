5670d4bc7beec3c72d7d95391c5f26666f1efc48
enjoy-peach <13270358502@163.com>
2020-04-24 14:36:50 +0800

    1.测试

---
+++ src/test/java/com/new_tx/mi/MiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.new_tx.mi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
